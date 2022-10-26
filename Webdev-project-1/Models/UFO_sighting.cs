31864e1cf012d28721e77f9064fcc813f35ab5e5
Skandinaver <Petter258@gmail.com>
2022-10-26 20:53:57 +0200

    Column display names fixed

--- Webdev-project-1/Models/UFO_sighting.cs
+++ Webdev-project-1/Models/UFO_sighting.cs
@@ -10,13 +10,17 @@ namespace Webdev_project_1.Models
         public int ID { get; set; }
         public int? CategoryID { get; set; }
         public Category? Category { get; set; }
+        [Display(Name = "Subject")]
         public string? UFO_title { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
+        [Display(Name = "Longitude")]
         public  decimal longitude { get; set; }
+        [Display(Name = "Latitude")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal latitude { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Observation Date")]
         public DateTime Observation_date { get; set; }
         public string? Description { get; set; }
 
